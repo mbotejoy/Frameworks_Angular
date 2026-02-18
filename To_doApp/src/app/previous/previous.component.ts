7f683afa9d64c3842c16d86c1dc525fa63eb2947
mbotejoy <joy.mbote@strathmore.edu>
2026-02-18 12:52:42 +0300

    added all the necessary components

---
+++ To_doApp/src/app/previous/previous.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous',
+  imports: [],
+  templateUrl: './previous.component.html',
+  styleUrl: './previous.component.css',
+})
+export class PreviousComponent {
+
+}
