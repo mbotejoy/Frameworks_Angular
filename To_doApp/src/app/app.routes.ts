732a3adb632c1f81df762a4141a34966fe1802cc
mbotejoy <joy.mbote@strathmore.edu>
2026-02-19 09:22:30 +0300

    updated the navigation links

--- To_doApp/src/app/app.routes.ts
+++ To_doApp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import{LoginComponent} from './login/login.component';
+import{SignupComponent} from './signup/signup.component';
+import{CreateComponent} from './create/create.component';
+import{ListComponent} from './list/list.component';
+import{PreviousComponent} from './previous/previous.component';         
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'create',component:CreateComponent},
+    {path:'list',component:ListComponent},
+    {path:'previous',component:PreviousComponent},
+    {path:'',redirectTo:'/login',pathMatch:'full'}
+];
